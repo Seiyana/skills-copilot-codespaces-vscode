86df7710e354969fdb374a00ee23f0295b16b5b7
Martin Josh de Leon <martinjosh.deleon@tup.edu.ph>
2026-03-16 06:23:59 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,4 +1,4 @@
-function CalculateNumbers(var1, var2) {
+function calculateNumbers(var1, var2) {
     console.log("Calculating numbers...");
     var result = var1 + var2;
     console.log("The result is: " + result);
